e53f4fa51c151624d9f514f129ca26ed6ded1c52
Zidane Ahmad <zidaneahmad456@gmail.com>
2024-08-06 20:53:03 +0700

    kumpul

--- JURNAL/jurnal11/soal2.java
+++ JURNAL/jurnal11/soal2.java
@@ -1,6 +1,24 @@
 package jurnal11;
+
+import java.util.Scanner;
+
 public class soal2 {
     public static void main (String [] args){
-        
+        Scanner s = new Scanner(System.in);
+
+        int angka1 = s.nextInt();
+        int angka2 = s.nextInt();
+
+        // Menghitung FPB 
+        while (angka2 != 0) {
+            int temp = angka2;
+            angka2 = angka1 % angka2;
+            angka1 = temp;
+        }
+
+        System.out.println(angka1);
     }
 }
+
+    
+
